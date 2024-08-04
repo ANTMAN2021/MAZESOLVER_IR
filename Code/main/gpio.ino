955ec65a4fbd2e57065cb269fca497f2d43cc109
ANTMAN2021 <87193314+ANTMAN2021@users.noreply.github.com>
2024-08-04 11:40:59 +0530

    Create gpio.ino

---
+++ Code/main/gpio.ino
@@ -0,0 +1,14 @@
+void gpioInit(void){
+  pinMode(m1, OUTPUT);
+  pinMode(m2, OUTPUT);
+  pinMode(m3, OUTPUT);
+  pinMode(m4, OUTPUT);
+  pinMode(e1, OUTPUT);
+  pinMode(e2, OUTPUT);
+  pinMode(IR, INPUT_PULLUP);
+  pinMode(IR2, INPUT_PULLUP);
+  pinMode(IR3, INPUT_PULLUP);
+  pinMode(IR4, INPUT_PULLUP);
+  pinMode(IR5, INPUT_PULLUP);
+    
+}
